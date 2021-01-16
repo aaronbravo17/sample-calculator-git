6571dbe37769d80466a7732c306c55b4030b1497
Aaron Bravo <aaron.bravo@epn.edu.ec>
2021-01-16 06:10:22 -0800

    Adding calculator execute class. Adding calculator java file jar

---
+++ src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
@@ -0,0 +1,16 @@
+package ec.edu.epn.git.calculator;
+
+public class CalculatorExecute {
+
+    public static void main(String[] args){
+
+        System.out.println("Calculator execute");
+        Calculator c = new Calculator();
+        int addition = c.addition(4,7);
+        System.out.println("Addition: " + addition);
+
+        int substraction = c.subtraction(7,1);
+        System.out.println("Subtraction: " + substraction);
+
+    }
+}
